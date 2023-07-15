725e71e6f78ad37c58ae3d761777a81a877fe311
Ohffs <ohffsnotnow@gmail.com>
2023-07-15 13:36:15 +0100

    Refactored the main code to make it a bit easier to read and less error prone

--- paper_reducer.py
+++ paper_reducer.py
@@ -11,16 +11,22 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-prompts = [
-    "Give me a very clear explanation of the core assertions, implications, and mechanics elucidated in this paper?",
-    "Can you explain the value of this in basic terms? Like you're talking to a CEO. So what? What's the bottom line here?",
-    "Can you give me an analogy or metaphor that will help explain this to a broad audience.",
-]
-default_max_chars = int(os.getenv('REDUCER_MAX_CHARS', 12000))
-default_max_token_spend = int(os.getenv('REDUCER_MAX_TOKEN_SPEND', 12000))
-default_model_engine = os.getenv('REDUCER_MODEL', "gpt-3.5-turbo-16k")
-default_temperature = float(os.getenv('REDUCER_TEMPERATURE', 0.1))
-default_personality = os.getenv('REDUCER_PERSONALITY', "You are a helpful AI assistant that is expert in taking in complex information and summarising it in a clear, friendly, concise accurate and informative way.")
+# Defined as constants
+MAX_TRIES = 5
+PAPERS_DIR = 'papers'
+# Use a configuration dictionary to manage settings
+config = {
+    "prompts": [
+        "Give me a very clear explanation of the core assertions, implications, and mechanics elucidated in this paper?",
+        "Can you explain the value of this in basic terms? Like you're talking to a CEO. So what? What's the bottom line here?",
+        "Can you give me an analogy or metaphor that will help explain this to a broad audience.",
+    ],
+    "max_chars": int(os.getenv('REDUCER_MAX_CHARS', 12000)),
+    "max_token_spend": int(os.getenv('REDUCER_MAX_TOKEN_SPEND', 12000)),
+    "model_engine": os.getenv('REDUCER_MODEL', "gpt-3.5-turbo-16k"),
+    "temperature": float(os.getenv('REDUCER_TEMPERATURE', 0.1)),
+    "personality": os.getenv('REDUCER_PERSONALITY', "You are a helpful AI assistant that is expert in taking in complex information and summarising it in a clear, friendly, concise accurate and informative way.")
+}
 
 def get_available_models():
     openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -28,12 +34,11 @@ def get_available_models():
     for model in models['data']:
         print(model.id)
 
-def string_to_token_count(string, model_engine=default_model_engine):
-    encoding = tiktoken.encoding_for_model(model_engine)
+def string_to_token_count(string):
+    encoding = tiktoken.encoding_for_model(config['model_engine'])
     return len(encoding.encode(string))
 
-def get_token_price(model=default_model_engine, token_count=0, direction="output"):
-
+def get_token_price(model = config['model_engine'], token_count=0, direction="output"):
     if model.startswith('gpt-4-32k'):
         token_price_input = 0.03 / 1000
         token_price_output = 0.06 / 1000
@@ -54,7 +59,7 @@ def get_token_price(model=default_model_engine, token_count=0, direction="output
         return round(token_price_input * token_count, 2)
     return round(token_price_output * token_count, 2)
 
-def get_new_pdf_contents(papers_dir, max_chars=default_max_chars):
+def get_new_pdf_contents(papers_dir, max_chars=config['max_chars']):
     results = {}
     for filename in os.listdir(papers_dir):
         if filename.endswith('.pdf'):
@@ -74,59 +79,114 @@ def get_new_pdf_contents(papers_dir, max_chars=default_max_chars):
                 results[filename] = text_contents
     return results
 
-def call_openai_api(results, prompts, model_engine=default_model_engine, temperature=default_temperature, max_token_spend=default_max_token_spend, personality=default_personality):
+def call_openai_api(results: dict, config: dict) -> dict:
+    """
+    Calls the OpenAI API and returns the responses.
+
+    Args:
+        results: A dictionary with filename as key and contents as value.
+        config: A dictionary with configurations.
+
+    Returns:
+        A dictionary with filename as key and API response as value.
+    """
     openai.api_key = os.environ["OPENAI_API_KEY"]
     api_responses = {}
     tokens_spent = 0
+    stop_processing = False
     for filename, contents in results.items():
+        if stop_processing:
+            break
         estimated_token_count = string_to_token_count(contents)
         print("Processing {} (estimated {} tokens)...".format(filename, estimated_token_count))
         combined_responses = []
-        max_tries = 5
-        try_count = 0
-        for index, prompt in enumerate(prompts):
-            messages = [
-                {
-                    'role': 'system',
-                    'content': f"{personality}. Here is the text to summarise as instructed by the user: {contents}",
-                },
-                {
-                    'role': 'user',
-                    'content': prompt,
-                }
-            ]
-            while try_count < max_tries:
-                spinner = Halo(text=f'Thinking about prompt {index + 1}...', spinner='dots')
-                spinner.start()
-                try_count += 1
-                try:
-                    response = openai.ChatCompletion.create(
-                        model=model_engine,
-                        messages=messages,
-                        temperature=temperature,
-                    )
-                    tokens = response['usage']['total_tokens']
-                    print("Response received : tokens used: {} | Estimated cost US${}".format(tokens, get_token_price(model_engine, tokens, "output")))
-                    tokens_spent += tokens
-                    break
-                except Exception as e:
-                    print("Error calling OpenAI API: {}".format(e))
-                    if try_count < max_tries:
-                        print("Retrying in {} seconds...".format(try_count * 2))
-                        sleep(try_count * 2)
-                finally:
-                    spinner.stop()
-            if try_count >= max_tries:
-                print("Failed to call OpenAI API after {} tries. Skipping {}.".format(max_tries, filename))
+        for index, prompt in enumerate(config["prompts"]):
+            response, tokens = get_openai_response(contents, config, prompt, index)
+            if response:
+                combined_responses.append("\n\n## Q. {}\n\n{}".format(prompt, response['choices'][0]['message']['content']))
+            tokens_spent += tokens
+            if config["max_token_spend"] > 0 and tokens_spent > config["max_token_spend"]:
+                print(f"Max tokens ({config['max_token_spend']}) exceeded. Skipping remaining files...")
+                stop_processing = True
                 break
-            try_count = 0
-            combined_responses.append("\n\n## Q. {}\n\n{}".format(prompt, response['choices'][0]['message']['content']))
-        if len(combined_responses) > 0:
+        if combined_responses:
             api_responses[filename] = "# {}\n\n".format(filename) + '\n'.join(combined_responses)
-        if max_token_spend > 0 and tokens_spent > max_token_spend:
-            print(f"Max tokens ({max_token_spend}) exceeded. Skipping remaining files...")
-            break
     return api_responses
+
+def get_openai_response(contents: str, config: dict, prompt: str, index: int) -> tuple:
+    """
+    Calls the OpenAI API with retry logic.
+
+    Args:
+        contents: The contents to summarize.
+        config: A dictionary with configurations.
+        prompt: The prompt to use.
+        index: The index of the prompt.
+
+    Returns:
+        A tuple containing the response and tokens used.
+    """
+    messages = [
+        {
+            'role': 'system',
+            'content': f"{config['personality']}. Here is the text to summarise as instructed by the user: {contents}",
+        },
+        {
+            'role': 'user',
+            'content': prompt,
+        }
+    ]
+    for try_count in range(MAX_TRIES):
+        spinner = Halo(text=f'Thinking about prompt {index + 1}...', spinner='dots')
+        spinner.start()
+        try:
+            response = openai.ChatCompletion.create(
+                model=config['model_engine'],
+                messages=messages,
+                temperature=config['temperature'],
+            )
+            tokens = response['usage']['total_tokens']
+            print("Response received : tokens used: {} | Estimated cost US${}".format(tokens, get_token_price(config['model_engine'], tokens, "output")))
+            return response, tokens
+        except Exception as e:
+            print(f"Error calling OpenAI API: {e}")
+            if try_count < MAX_TRIES - 1:
+                print(f"Retrying in {try_count * 2} seconds...")
+                sleep(try_count * 2)
+        finally:
+            spinner.stop()
+    print("Failed to call OpenAI API after {} tries. Skipping {}.".format(MAX_TRIES, filename))
+    return None, 0
+
+def handle_webpage_summary(url: str, config: dict):
+    """
+    Handles the summarization of a webpage.
+
+    Args:
+        url: The URL of the webpage to summarize.
+        config: A dictionary with configurations.
+    """
+    results = {}
+    filename = re.sub(r"^https?://", "", url, flags=re.IGNORECASE)
+    filename = filename.replace(".", "_")
+    filename = re.sub(r"/|_", "_", filename)
+    filepath = os.path.join(PAPERS_DIR, filename)
+    if os.path.exists(filepath + '_summary.md'):
+        print("Skipping {} as it has already been processed...".format(filename))
+        return
+    print("Summarising {}".format(url))
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    page_text = soup.get_text(strip=True)
+
+    if config["max_chars"] > 0 and len(page_text) > int(config["max_chars"]):
+        print("Truncating {} to {} characters...".format(filename, config["max_chars"]))
+        page_text = page_text[0:config["max_chars"]]
+    results[filename] = page_text
+    api_responses = call_openai_api(results, config)
+    for filename, response in api_responses.items():
+        filepath = os.path.join(PAPERS_DIR, filename + '_summary.md')
+        save_file(filepath, response)
 
 # file operations
 def save_file(filepath, content):
@@ -139,39 +199,15 @@ if __name__ == '__main__':
     parser.add_argument('--url', help='URL of a webpage to summarise')
     args = parser.parse_args()
 
-    # If the --models flag was passed, display the models available with the users API key
     if args.models:
         get_available_models()
         exit(0)
 
-    # If the --url flag was passed, summarise the webpage
     if args.url:
-        url = args.url
-        results = {}
-        filename = re.sub(r"^https?://", "", url, flags=re.IGNORECASE)
-        filename = filename.replace(".", "_")
-        filename = re.sub(r"/|_", "_", filename)
-        filepath = os.path.join('papers', filename)
-        if os.path.exists(filepath + '_summary.md'):
-            print("Skipping {} as it has already been processed...".format(filename))
-            exit(0)
-        print("Summarising {}".format(url))
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        page_text = soup.get_text(strip=True)
-
-        if default_max_chars > 0 and len(page_text) > int(default_max_chars):
-            print("Truncating {} to {} characters...".format(filename, default_max_chars))
-            text_contents = page_text[0:default_max_chars]
-        results[filename] = page_text
-        api_responses = call_openai_api(results, prompts)
+        handle_webpage_summary(args.url, config)
+    else:
+        results = get_new_pdf_contents(PAPERS_DIR, config["max_chars"])
+        api_responses = call_openai_api(results, config)
         for filename, response in api_responses.items():
-            filepath = os.path.join('papers', filename + '_summary.md')
+            filepath = os.path.join(PAPERS_DIR, filename + '_summary.md')
             save_file(filepath, response)
-        exit(0)
-    papers_dir = 'papers'
-    results = get_new_pdf_contents(papers_dir)
-    api_responses = call_openai_api(results, prompts)
-    for filename, response in api_responses.items():
-        filepath = os.path.join(papers_dir, filename + '_summary.md')
-        save_file(filepath, response)
